5c796b0b9e1c776b98e06b539e542e9d4d5db63f
Coyote <sturnerprop@yahoo.com>
2021-09-27 17:33:51 -0500

    Initial Commit

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main(String[] args){
+        System.out.println("My name is Sean Turner");
+        System.out.println("I am 29");
+        System.out.println("My hometown is Oklahoma City, Oklahoma");
+    }
+}
